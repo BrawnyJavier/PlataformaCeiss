21747c4a77907cabba94b23fb14f0944b874c06c
LAPTOP-OM58TUO9\brawn <brawnyjavier@gmail.com>
2017-11-04 17:31:32 -0400

    Initial

---
+++ Datamodel/Datamodels.cs
@@ -0,0 +1,15 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datamodel
+{
+    public class CEISSContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
